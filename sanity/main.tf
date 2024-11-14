89157d969c1c7ce185544d2ab256f173459d87e7
Willk111 <wkingston20@gmail.com>
2024-11-14 10:48:11 +0000

    "Main.tf" clean up

--- sanity/main.tf
+++ sanity/main.tf
@@ -15,14 +15,14 @@ resource "aws_s3_bucket" "annysahbucket" {
   bucket = "annysah-website-practice"
 }
 
-resource "aws_s3_account_public_access_block" "bucket_access_block" {
-  bucket = aws_s3_bucket.bucket.id
-
-  block_public_acls = false
-  block_public_policy = false
-  ignore_public_acls = false
-  restrict_public_buckets = false
-}
+#resource "aws_s3_account_public_access_block" "bucket_access_block" {
+#  bucket = aws_s3_bucket.annysahbucket.id#
+#
+#  block_public_acls = false
+#  block_public_policy = false
+#  ignore_public_acls = false
+#  restrict_public_buckets = false
+#}
 
 
 resource "tls_private_key" "priv_key" {
@@ -40,37 +40,6 @@ resource "local_file" "ssh_key" {
   content  = tls_private_key.priv_key.private_key_pem
 }
 
-resource "aws_vpc" "new" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-}
-
-resource "aws_subnet" "new_subnet" {
-  vpc_id                  = aws_vpc.new.id
-  cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = true  
-}
-
-resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh"
-  vpc_id      = aws_vpc.new.id
-
-  ingress {
-    description = "Allow SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Change to a specific IP range if security is a concern
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
 
 resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.new.id
@@ -79,21 +48,7 @@ resource "aws_internet_gateway" "gw" {
   }
 }
 
-resource "aws_eip" "ip-test" {
-  instance = aws_instance.app_server.id
-}
 
-resource "aws_route_table" "route-table-test-env" {
-  vpc_id = "${aws_vpc.new.id}"
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.gw.id}"
-  }
-}
-resource "aws_route_table_association" "subnet-association" {
-  subnet_id      = "${aws_subnet.new_subnet.id}"
-  route_table_id = "${aws_route_table.route-table-test-env.id}"
-}
 
 resource "aws_instance" "app_server" {
   ami                 = "ami-0182f373e66f89c85"
